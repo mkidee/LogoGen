6dba940151bc1c4e06dab46ab648fbdeeb7d8230
mkidee <mckaymemmott@gmail.com>
2023-04-30 16:40:16 -0600

    add comments and exported shapes

--- library/shapes.js
+++ library/shapes.js
@@ -1,3 +1,4 @@
+// parent class of shape will pass a color type that all shapes would have in common
 class Shape {
     constructor(){
         this.color = "";
@@ -7,6 +8,7 @@ class Shape {
     }
 }
 
+//each individual class will return with its desired shape, and taking in an inputed color.
 class Circle extends Shape {
     render() {
         return "<circle cx='50%' cy='50%' r='100' width='100%' height='100%' fill='" + this.color + "' />;";
@@ -24,3 +26,6 @@ class Triangle extends Shape {
         return "<polygon points='150 0 25 200 275 200' height='100%' width='100%' fill='" + this.color + "' />";
     }
 }
+
+// exporting back to index
+module.exports = {Circle, Square, Triangle};
