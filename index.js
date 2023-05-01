c487ac77c4a2139dbd4449101d6e7968f5372f82
mkidee <mckaymemmott@gmail.com>
2023-05-01 11:51:48 -0600

    ran code to write to new file for SVG, also added parentheses where needed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // we need to include fs and inquirer as they are dependencies, as well we must include our shapes.js page for reference
-const fs = 'fs';
-const inquirer = 'inquirer';
+const fs = require('fs');
+const inquirer = require('inquirer');
 const {Circle, Square, Triangle} = require("./library/shapes");
 
 // choices for the logo selection
@@ -36,4 +36,35 @@ function writeSVG(data) {
     const logoSource = './createdlogo/logo.svg';
     let baseSVG = "";
     baseSVG = `<svg version='1.1' width='300' height='200' xmlns='http://www.w3.org/2000/svg'>`;
+
+    logoChar = data.characters;
+    logoShapeColor = data.shapeColor;
+    logoCharColor = data.characterColor;
+
+    let shapeChoice;
+    if (data.shape === "Circle") {
+        shapeChoice = new Circle();
+    }
+    else if (data.shape === "Square") {
+        shapeChoice = new Square();
+    }
+    else if (data.shape === "Triangle") {
+        shapeChoice = new Triangle();
+    }
+
+    shapeChoice.setColor(logoShapeColor);
+    baseSVG += shapeChoice.render();
+    
+    baseSVG += `<text x='150' y='115' text-anchor='middle' font-size='50' fill='${logoCharColor}'>${logoChar}</text>`;
+    baseSVG += '</svg>';
+
+    fs.writeFile(logoSource, baseSVG, function(err) {
+        err ? console.log(err) : console.log("Done! Logo was successfully created in the 'createdlogo' folder!")
+    });
+
+    function init() {
+        inquirer.prompt(questions).then(data => writeSVG(data));
+    }
+
+    init();
 }
